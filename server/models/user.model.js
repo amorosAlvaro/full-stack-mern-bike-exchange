7b95a5365aa64e51e3ba58bdbbd479c07b351d70
amorosAlvaro <91912828+amorosAlvaro@users.noreply.github.com>
2021-11-29 13:36:13 +0100

    add user Schema

---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+module.exports = mongoose.model(
+  'User',
+  Schema({
+    _id: Schema.Types.ObjectId,
+    name: {
+      String,
+      required: true,
+    },
+    email: {
+      String,
+      required: true,
+    },
+    province: {
+      String,
+      required: true,
+    },
+    phone: String,
+    bikes: Array,
+    favorites: Array,
+  })
+);
